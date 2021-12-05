6e6602465cfbd517340a952dda2e00c776fe3cae
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-12-05 11:25:52 -0300

    Windows Compatible

--- main.go
+++ main.go
@@ -25,39 +25,29 @@ func fileCompleter(shell *ishell.Shell, printEntries bool) func(string, []string
 	return func(searchPrefix string, searchTargets []string) (ret []string) {
 		searchPrefixParts := strings.Split(searchPrefix, "/")
 
-		// if the searchPrefix has a path in it, we need to do our searching in that path for that prefix
-		// otherwise, search here, either for the searchTargets, if they exist, or the non-location part of the searchPrefix
 		searchLocation := ""
 		searchTarget := ""
 		if len(searchPrefixParts) > 1 {
-			searchLocation = strings.Join(searchPrefixParts[0 : len(searchPrefixParts)-1], "/")
+			searchLocation = strings.Join(searchPrefixParts[0:len(searchPrefixParts)-1], "/")
 			searchTarget = searchPrefixParts[len(searchPrefixParts)-1]
 		}
 
-		// If there's explicit search targets being passed to the tab completion, respect those
 		if len(searchTargets) > 0 {
-			// if the search target *has* the path, the group name search will fail because it's only the name(title for entries)
-			// the search target is split on spaces
 			fullSearchTarget := strings.Join(searchTargets, " ")
 
-			// this will split the now fully joined path by slashed
 			searchTargetParts := strings.Split(fullSearchTarget, "/")
 
-			// This will make the search target itself be only the name
 			searchTarget = searchTargetParts[len(searchTargetParts)-1]
 
-			// unescape the spaces, the shell needs them, but they break search
 			searchTarget = strings.ReplaceAll(searchTarget, "\\ ", " ")
 
 			if searchLocation == "" {
-				searchLocation = strings.Join(searchTargetParts[0 : len(searchTargetParts)-1], "/")
+				searchLocation = strings.Join(searchTargetParts[0:len(searchTargetParts)-1], "/")
 			}
 		}
-		// now we find the location object for the current searchPrefix, as parsed above
 		db := shell.Get("db").(t.Database)
 		location, _, err := commands.TraversePath(db, db.CurrentLocation(), searchLocation)
 
-		// if the there was an error, return nothing
 		if err != nil {
 			return
 		}
@@ -66,14 +56,8 @@ func fileCompleter(shell *ishell.Shell, printEntries bool) func(string, []string
 			location = db.CurrentLocation()
 		}
 
-		// There's some duplication here, but basically the idea is that it will search every group and entry
-		// in the current location for anything starting with whatever the searchTarget came out to be
-		// There appear to be 2 modes in play, one where the user has entered an existing path, in which case the code is supposed to return the full path
-		// and one where the user has entered a partial path, in which case the code is supposed to return *only* the completion
-		// This drove me crazy. RIP my sanity. Argh.
-
 		returnedPrefix := ""
-		doCompletion := searchPrefix == "" || len (searchTargets) >= 1
+		doCompletion := searchPrefix == "" || len(searchTargets) >= 1
 		if searchLocation != "" {
 			returnedPrefix = searchLocation + "/"
 		}
@@ -85,7 +69,6 @@ func fileCompleter(shell *ishell.Shell, printEntries bool) func(string, []string
 			}
 
 			if searchTarget != "" && doCompletion {
-				// we have to return a completion, not a path. don't ask me why
 				completedName := strings.Replace(g.Name(), searchTarget, "", 1)
 				ret = append(ret, strings.TrimLeft(completedName, " "))
 				continue
@@ -99,7 +82,6 @@ func fileCompleter(shell *ishell.Shell, printEntries bool) func(string, []string
 					continue
 				}
 				if searchTarget != "" && doCompletion {
-					// we have to return a completion, not a path. don't ask me why
 					completedTitle := strings.Replace(e.Title(), searchTarget, "", 1)
 					ret = append(ret, strings.TrimLeft(completedTitle, " "))
 					continue
@@ -117,28 +99,25 @@ func buildVersionString() string {
 	return fmt.Sprintf("%s.%s-%s.%s (built on %s from %s)", VersionRelease, VersionBuildDate, VersionBuildTZ, VersionBranch, VersionHostname, VersionRevision)
 }
 
-// promptForDBPassword will determine the password based on environment vars or, lacking those, a prompt to the user
 func promptForDBPassword(shell *ishell.Shell) (string, error) {
-	// we are prompting for the password
 	shell.Print("enter database password: ")
 	return shell.ReadPasswordErr()
 }
 
-// newDB will create or open a DB with the parameters specified.  `open` indicates whether the DB should be opened or not (vs created)
 func newDB(dbPath string, password string, keyPath string, version int) (t.Database, error) {
 	var dbWrapper t.Database
 	switch version {
-		case 2:
-			dbWrapper = &v2.Database{}
-		case 1:
-			dbWrapper = &v1.Database{}
-		default:
-			return nil, fmt.Errorf("invalid version '%d'", version)
+	case 2:
+		dbWrapper = &v2.Database{}
+	case 1:
+		dbWrapper = &v1.Database{}
+	default:
+		return nil, fmt.Errorf("invalid version '%d'", version)
 	}
-	dbOpts := t.Options {
-		DBPath: dbPath,
+	dbOpts := t.Options{
+		DBPath:   dbPath,
 		Password: password,
-		KeyPath: keyPath,
+		KeyPath:  keyPath,
 	}
 	err := dbWrapper.Init(dbOpts)
 	return dbWrapper, err
@@ -160,7 +139,6 @@ func main() {
 		dbPath = *dbFile
 	}
 
-	// default to the flag argument
 	keyPath := *keyFile
 
 	if envKeyfile, found := os.LookupEnv("KP_KEYFILE"); found && keyPath == "" {
@@ -168,8 +146,6 @@ func main() {
 	}
 
 	for {
-		// if the password is coming from an environment variable, we need to terminate
-		// after the first attempt or it will fall into an infinite loop
 		var err error
 		password, passwordInEnv := os.LookupEnv("KP_PASSWORD")
 		if !passwordInEnv {
@@ -183,9 +159,6 @@ func main() {
 
 		dbWrapper, err = newDB(dbPath, password, keyPath, *keepassVersion)
 		if err != nil {
-			// typically, these errors will be a bad password, so we want to keep prompting until the user gives up
-			// if, however, the password is in an environment variable, we want to abort immediately so the program doesn't fall
-			// in to an infinite loop
 			shell.Printf("could not open database: %s\n", err)
 			if passwordInEnv {
 				os.Exit(1)
@@ -195,25 +168,6 @@ func main() {
 		break
 	}
 
-	if dbWrapper.Locked() {
-		shell.Printf("Lockfile exists for DB at path '%s', another process is using this database!\n", dbWrapper.SavePath())
-		shell.Printf("Open anyways? Data loss may occur. (will only proceed if 'yes' is entered)  ")
-		line, err := shell.ReadLineErr()
-		if err != nil {
-			shell.Printf("could not read user input: %s\n", line)
-			os.Exit(1)
-		}
-
-		if line != "yes" {
-			shell.Println("aborting")
-			os.Exit(1)
-		}
-	}
-
-	if err := dbWrapper.Lock(); err != nil {
-		shell.Printf("aborting, could not lock database: %s\n", err)
-		os.Exit(1)
-	}
 	shell.Printf("opened database at %s\n", dbWrapper.SavePath())
 
 	shell.Set("db", dbWrapper)
@@ -394,7 +348,6 @@ func main() {
 		shell.Run()
 	}
 
-	// This will run after the shell exits
 	fmt.Println("exiting")
 
 	if dbWrapper.Changed() {
@@ -406,9 +359,4 @@ func main() {
 		fmt.Println("no changes detected since last save.")
 	}
 
-
-	if err := dbWrapper.Unlock(); err != nil {
-		fmt.Printf("failed to unlock db: %s", err)
-		os.Exit(1)
-	}
 }
